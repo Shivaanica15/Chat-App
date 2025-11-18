91d88596e5f89102e76958aa31c3f245c63a1f4e
Shivaanica V <vasanthakumarshivaanica@gmail.com>
2025-11-18 12:27:46 +0530

    messages are displaying properly

--- frontend/components/ConversationItem.tsx
+++ frontend/components/ConversationItem.tsx
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { spacingX, spacingY } from '@/constants/theme'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import Avatar from './Avatar'
+import Typo from './Typo';
+import moment from 'moment';
 
-const ConversationItem = () =>{
+const ConversationItem = ({item, showDriver, router}: any) =>{
+
+    
+    const openConversation = () =>{}
+
+    const getLastMessageContent = () => {
+        if(!lastMessage) return " Say hi ⭐ ";
+
+        return lastMessage?.attachment ? "Image" : lastMessage.content;
+    }
+
+    const lastMessage: any = item.lastMessage;
+    const isDirect =item.type =='direct';
+
+    const getLastMessageData = () =>{
+        if(!lastMessage.createdAt) return null;
+
+        const messageDate = moment(lastMessage.createdAt);
+
+        const today = moment();
+
+        if(messageDate.isSame(today, "day")){
+            return messageDate.format("h:mm A");
+        }
+        if(messageDate.isSame(today, "year")){
+            return messageDate.format("MMM D");
+        }
+        
+            return messageDate.format("MMM D, YYYY");
+        
+    }
     return (
         <View>
-            <Text> ConversationItems </Text>
+    <TouchableOpacity style={styles.ConversationItem}>
+        <View>
+            <Avatar uri={null} size={47} isGroup={item.type == "group"} />
         </View>
-    )
-}
+
+        <View style={{ flex: 1 }}>
+            <Typo size={17} fontWeight={"600"}>
+                {item?.name}
+            </Typo>
+
+            {/* 👇 Swapped: last message preview now comes here */}
+            <Typo
+                size={15}
+                color={colors.neutral600}
+                textProps={{ numberOfLines: 1 }}
+            >
+                {getLastMessageContent()}
+            </Typo>
+        </View>
+
+        {/* 👇 Swapped: date/time now at right side */}
+        {item.lastMessage && (
+            <Typo size={15}>{getLastMessageData()}</Typo>
+        )}
+    </TouchableOpacity>
+</View>
+
+    );
+};
 
 export default ConversationItem
 
