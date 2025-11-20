8c49fe0a37b2b5262ef4a6e02ee606dcaf90351e
Shivaanica V <vasanthakumarshivaanica@gmail.com>
2025-11-20 16:34:41 +0530

    conversation css

--- frontend/app/(main)/conversation.tsx
+++ frontend/app/(main)/conversation.tsx
@@ -1,15 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper';
-import { colors } from '@/constants/theme';
+import { colors, spacingY } from '@/constants/theme';
 import Typo from '@/components/Typo';
 import { useLocalSearchParams } from 'expo-router';
+import { useAuth } from '@/contexts/authContext';
+import Avatar from '@/components/Avatar';
+import { verticalScale } from '@/utils/styling';
 
 const Conversation = () => {
 
-    const data = useLocalSearchParams();
+    const {user: currentUser} = useAuth();
 
-    console.log("got conversation data: ", data);
+    const {
+        id: conversationId,
+        name,
+        participants: stringifiedParticipants,
+        avatar,
+        type
+    } = useLocalSearchParams();
+
+    const participants = stringifiedParticipants
+        ? JSON.parse(stringifiedParticipants as string)
+        : [];
+
+    let conversationAvatar = avatar;
+
+    let isDirect = type == 'direct';
+    const otherParticipant = isDirect ? participants.find((p:any)=>p.id != currentUser?.id) : null;
+
+    if (isDirect && otherParticipant)
+        conversationAvatar = otherParticipant.avatar;
+
+    let conversationName = isDirect && otherParticipant ? otherParticipant.name : name;
+
     return (
         <ScreenWrapper>
             <Typo color={colors.white}>Conversation</Typo>
@@ -19,4 +43,23 @@ const Conversation = () => {
 
 export default Conversation
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    footer:{
+        paddingTop: spacingY._7,
+        paddingBottom: verticalScale(22)
+    },
+    messagesContainer:{
+        flex:1,
+    },
+    messagesContent:{
+        paddingTop: spacingY._20,
+        paddingBottom: spacingY._10,
+        gap: spacingY._12,  
+    },
+    plusIcon:{
+        backgroundColor:colors.primary,
+        borderRadius: radius.full,
+        padding: 8,
+    },
+    
+})
